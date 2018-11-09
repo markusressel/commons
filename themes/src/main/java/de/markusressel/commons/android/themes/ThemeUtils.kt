d0b99980083ebe1402f4ecc4d462f62029ef6cf4
Markus Ressel <mail@markusressel.de>
2018-11-09 20:47:55 +0100

    removed app module created core module build.gradle cleanup changed ThemeUtil methods to kotlin extensions

---
+++ themes/src/main/java/de/markusressel/commons/android/themes/ThemeUtils.kt
@@ -0,0 +1,42 @@
+package de.markusressel.commons.android.themes
+
+import android.app.Activity
+import android.content.Context
+import android.util.TypedValue
+import androidx.annotation.AttrRes
+import androidx.annotation.ColorInt
+import androidx.annotation.StyleRes
+import androidx.core.content.ContextCompat
+
+/**
+ * Get Color from Theme attribute
+ *
+ * @param context Activity context
+ * @param attr    Attribute ressource ID
+ *
+ * @return Color as Int
+ */
+@ColorInt
+fun Context.getThemeAttrColor(@AttrRes attr: Int): Int {
+    val typedValue = TypedValue()
+    if (this.theme.resolveAttribute(attr, typedValue, true)) {
+        if (typedValue.type >= TypedValue.TYPE_FIRST_INT && typedValue.type <= TypedValue.TYPE_LAST_INT) {
+            return typedValue.data
+        } else if (typedValue.type == TypedValue.TYPE_STRING) {
+            return ContextCompat.getColor(this, typedValue.resourceId)
+        }
+    }
+
+    return 0
+}
+
+/**
+ * Apply a theme to an activity
+ *
+ * @param activity the activity to apply the theme to
+ * @param themeRes theme resource
+ */
+fun Activity.applyTheme(@StyleRes themeRes: Int) {
+    this.applicationContext.setTheme(themeRes)
+    this.setTheme(themeRes)
+}
