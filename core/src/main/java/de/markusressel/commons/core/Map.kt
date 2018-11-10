391811760774e08aba76677ee1a39e1c642f902d
Markus Ressel <mail@markusressel.de>
2018-11-10 01:21:33 +0100

    updated gradle to latest version upped minSdk to 19 added Map class

---
+++ core/src/main/java/de/markusressel/commons/core/Map.kt
@@ -0,0 +1,17 @@
+package de.markusressel.commons.core
+
+import java.util.*
+
+/**
+ * Map.forEach as an extension function (to be able to use it on API Levels < 24
+ */
+fun <K : Any, V : Any> Map<K, V>.forEachCompat(action: ((k: K, v: V) -> Unit)) {
+    for (entry in entries) {
+        try {
+            action(entry.key, entry.value)
+        } catch (ise: IllegalStateException) {
+            // this usually means the entry is no longer in the map.
+            throw ConcurrentModificationException(ise)
+        }
+    }
+}
